03636aada4cebe6efe2650d3d2126dd1c39659fd
jbonaiuto <jbonaiuto@eigenminds.com>
2013-12-30 19:39:50 +0000

    Update settings.py
    changed settings for server

--- uscbp/settings.py
+++ uscbp/settings.py
@@ -7,7 +7,6 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     ('James Bonaiuto', 'bonaiuto@caltech.edu'),
-    ('Rob Schuler', 'schuler@isi.edu'),
 )
 
 MANAGERS = ADMINS
@@ -15,10 +14,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'uscbp_new',                      # Or path to database file if using sqlite3.
+        'NAME': 'uscbp',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': '4uhk48s3',
+        'PASSWORD': 'ilnyij*02',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -60,7 +59,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/jbonaiuto/Projects/bodb/new_version/bodb/media/'
+MEDIA_ROOT = '/var/www/bodb/bodb/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -71,7 +70,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/jbonaiuto/Projects/bodb/new_version/bodb/static'
+STATIC_ROOT = '/var/www/bodb/bodb/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -125,8 +124,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/jbonaiuto/Projects/bodb/new_version/uscbp/templates',
-    '/home/jbonaiuto/Projects/bodb/new_version/bodb/templates',
+    '/var/www/bodb/uscbp/templates',
+    '/var/www/bodb/bodb/templates',
 )
 
 INSTALLED_APPS = (
