faf52680e6c706086f8388d03b03ab3848114a4a
jbonaiuto <jbonaiuto@gmail.com>
2014-01-02 12:46:46 +0000

    data to initialize db with

---
+++ bodb/sql/atlas.sql
@@ -0,0 +1,5 @@
+INSERT INTO bodb_atlas (file) VALUES ('mniHuman.xml');
+INSERT INTO bodb_atlas (file) VALUES ('paxinosWatson.xml');
+INSERT INTO bodb_atlas (file) VALUES ('talairach.xml');
+INSERT INTO bodb_atlas (file) VALUES ('talairachMRIAvg.xml');
+INSERT INTO bodb_atlas (file) VALUES ('talairachMRISingle.xml');
---
+++ bodb/sql/coordinatespace.sql
@@ -0,0 +1,4 @@
+INSERT INTO bodb_coordinatespace (name) VALUES ('Stereotaxic');
+INSERT INTO bodb_coordinatespace (name) VALUES ('Talairach');
+INSERT INTO bodb_coordinatespace (name) VALUES ('MNI');
+INSERT INTO bodb_coordinatespace (name) VALUES ('MNIMonkey');
