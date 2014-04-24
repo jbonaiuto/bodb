d46223891181d54290c54d18a675bdd6b95899d5
jbonaiuto <jbonaiuto@gmail.com>
2014-04-24 18:12:44 +0100

    fixed api permissions for brain region

--- bodb/permissions.py
+++ bodb/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from bodb.models import Document
+from bodb.models import Document, BrainRegion
 
 
 class IsEditorOrReadOnly(permissions.BasePermission):
@@ -12,28 +12,37 @@ class IsEditorOrReadOnly(permissions.BasePermission):
         # so we'll always allow GET, HEAD or OPTIONS requests.
         user=request.user
         if request.method in permissions.SAFE_METHODS:
-            if user.is_authenticated() and not user.is_anonymous():
-                if not user.is_superuser:
-                    in_group=False
-                    for group in user.groups.all():
-                        for collator_group in obj.collator.groups.all():
-                            if group.id==collator_group.id:
-                                in_group=True
-                                break
-                    if obj.collator==user or obj.public==1 or (obj.draft==0 and in_group):
-                        return True
-                else:
-                    return True
-            elif obj.public==1:
+            if type(obj) is BrainRegion:
                 return True
-            return False
+            else:
+                if user.is_authenticated() and not user.is_anonymous():
+                    if not user.is_superuser:
+                        in_group=False
+                        for group in user.groups.all():
+                            for collator_group in obj.collator.groups.all():
+                                if group.id==collator_group.id:
+                                    in_group=True
+                                    break
+                        if obj.collator==user or obj.public==1 or (obj.draft==0 and in_group):
+                            return True
+                    else:
+                        return True
+                elif obj.public==1:
+                    return True
+                return False
 
 
         elif request.method == "PUT" or request.method == "PATCH":
-            return user.is_authenticated() and (obj.collator==user or user.is_superuser or
-                                                user.has_perm('edit',Document.objects.get(id=obj.id)))
+            if type(obj) is BrainRegion:
+                return user.is_authenticated() and user.is_superuser
+            else:
+                return user.is_authenticated() and (obj.collator==user or user.is_superuser or
+                                                    user.has_perm('edit',Document.objects.get(id=obj.id)))
         elif request.method == "DELETE":
-            return user.is_authenticated() and (obj.collator==user or user.is_superuser or
+            if type(obj) is BrainRegion:
+                return user.is_authenticated() and user.is_superuser
+            else:
+                return user.is_authenticated() and (obj.collator==user or user.is_superuser or
                                                 user.has_perm('delete',Document.objects.get(id=obj.id)))
         else:
             return False
