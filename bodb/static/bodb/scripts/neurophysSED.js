caefc6c5e562a19bb8f83139917c422ea7c80e5a
jbonaiuto <jbonaiuto@gmail.com>
2014-10-21 07:08:59 +0100

    added realign-all

--- bodb/static/bodb/scripts/neurophysSED.js
+++ bodb/static/bodb/scripts/neurophysSED.js
@@ -1,8 +1,10 @@
+var last_to_realign='';
 
 function realignUnitConditionPlot(unit_id, condition_id, csrf_token)
 {
     document.getElementById('realigningMsg').style.display='block';
     document.getElementById('realigningOver').style.display='block';
+    last_to_realign='unit_'+unit_id+'_condition_'+condition_id;
     var event=document.getElementById('unit_'+unit_id+'_condition_'+condition_id+'_realign').value;
     var data={'unit': unit_id, 'condition': condition_id, 'event': event, 'csrfmiddlewaretoken': csrf_token};
     var args={type:"POST", url:"/bodb/sed/neurophysiology/unit_realign/", data: data, complete: doneRealignUnitConditionPlot };
@@ -17,8 +19,11 @@ function doneRealignUnitConditionPlot(res, status)
     if (status=="success")
     {
         document.getElementById('unit_'+data.unit+'_condition_'+data.condition+'_plot').src=data.diagram_url;
-        document.getElementById('realigningMsg').style.display='none';
-        document.getElementById('realigningOver').style.display='none';
+        if('unit_'+data.unit+'_condition_'+data.condition==last_to_realign)
+        {
+            document.getElementById('realigningMsg').style.display='none';
+            document.getElementById('realigningOver').style.display='none';
+        }
     }
     else
         alert(txt);
