c82bc38ed036704adcc08b4d5392cd004f80df13
jbonaiuto <jbonaiuto@gmail.com>
2015-09-01 12:47:04 +0100

    upgraded to google recaptcha v2. fixes #123

--- uscbp/urls.py
+++ uscbp/urls.py
@@ -3,11 +3,13 @@ from bodb.forms.admin import BodbRegistrationForm
 from bodb.views.admin import UpdateUserProfileView, BodbRegistrationView
 from django.conf import settings
 from django.conf.urls.static import static
+from registration.views import gRecaptchaVerify
 
 urlpatterns = patterns('',
     (r'^bodb/', include('bodb.urls')),
     (r'^accounts/logout/$', 'bodb.views.admin.logout_view', ),
     (r'^accounts/register/$', BodbRegistrationView.as_view(form_class=BodbRegistrationForm), {}, 'registration_register'),
+    (r'^accounts/verify/$', gRecaptchaVerify.as_view(), {}, 'grecaptcha_verify'),
     (r'^accounts/', include('registration.backends.default.urls')),
     (r'^accounts/username_available/$', 'bodb.views.admin.username_available', ),
     (r'^accounts/profile/$', UpdateUserProfileView.as_view(), {}, 'create_user_profile'),
