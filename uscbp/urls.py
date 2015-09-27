625dd978cbed738903c03e7ce13207f20518eca9
jbonaiuto <jbonaiuto@gmail.com>
2015-09-27 20:58:34 +0100

    try to fix account activation

--- uscbp/urls.py
+++ uscbp/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from bodb.forms.admin import BodbRegistrationForm
 from bodb.views.admin import UpdateUserProfileView, BodbRegistrationView
 from django.conf import settings
 from django.conf.urls.static import static
+from registration.backends.default.views import ActivationView
 from registration.views import gRecaptchaVerify
 
 if getattr(settings, 'ASKBOT_MULTILINGUAL', False) == True:
@@ -20,6 +22,16 @@ urlpatterns += patterns('',
     (r'^accounts/logout/$', 'bodb.views.admin.logout_view', ),
     (r'^accounts/register/$', BodbRegistrationView.as_view(form_class=BodbRegistrationForm), {}, 'registration_register'),
     (r'^accounts/verify/$', gRecaptchaVerify.as_view(), {}, 'grecaptcha_verify'),
+    url(r'^activate/complete/$',
+        TemplateView.as_view(template_name='registration/activation_complete.html'),
+        name='registration_activation_complete'),
+    # Activation keys get matched by \w+ instead of the more specific
+    # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+    # that way it can return a sensible "invalid key" message instead of a
+    # confusing 404.
+    url(r'^activate/(?P<activation_key>\w+)/$',
+        ActivationView.as_view(),
+        name='registration_activate'),
     (r'^accounts/', include('registration.backends.default.urls')),
     (r'^accounts/username_available/$', 'bodb.views.admin.username_available', ),
     (r'^accounts/profile/$', UpdateUserProfileView.as_view(), {}, 'create_user_profile'),
