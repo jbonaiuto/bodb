9bc8a3448c38d2a785377c666e6dd0f7bfd87d5e
jbonaiuto <jbonaiuto@gmail.com>
2015-09-27 20:55:38 +0100

    try to fix account activation

--- uscbp/urls.py
+++ uscbp/urls.py
@@ -5,17 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from registration.views import gRecaptchaVerify
 
-urlpatterns = patterns('',
-    (r'^bodb/', include('bodb.urls')),
-    (r'^accounts/logout/$', 'bodb.views.admin.logout_view', ),
-    (r'^accounts/register/$', BodbRegistrationView.as_view(form_class=BodbRegistrationForm), {}, 'registration_register'),
-    (r'^accounts/verify/$', gRecaptchaVerify.as_view(), {}, 'grecaptcha_verify'),
-    (r'^accounts/', include('registration.backends.default.urls')),
-    (r'^accounts/username_available/$', 'bodb.views.admin.username_available', ),
-    (r'^accounts/profile/$', UpdateUserProfileView.as_view(), {}, 'create_user_profile'),
-    url(r'^comments/', include('django.contrib.comments.urls')),
-)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 if getattr(settings, 'ASKBOT_MULTILINGUAL', False) == True:
     from django.conf.urls.i18n import i18n_patterns
     urlpatterns = i18n_patterns('',
@@ -26,3 +15,13 @@ else:
         (r'%s' % settings.ASKBOT_URL, include('askbot.urls'))
     )
 
+urlpatterns += patterns('',
+    (r'^bodb/', include('bodb.urls')),
+    (r'^accounts/logout/$', 'bodb.views.admin.logout_view', ),
+    (r'^accounts/register/$', BodbRegistrationView.as_view(form_class=BodbRegistrationForm), {}, 'registration_register'),
+    (r'^accounts/verify/$', gRecaptchaVerify.as_view(), {}, 'grecaptcha_verify'),
+    (r'^accounts/', include('registration.backends.default.urls')),
+    (r'^accounts/username_available/$', 'bodb.views.admin.username_available', ),
+    (r'^accounts/profile/$', UpdateUserProfileView.as_view(), {}, 'create_user_profile'),
+    url(r'^comments/', include('django.contrib.comments.urls')),
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
