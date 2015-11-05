d5d548a9701e7661a280dec290eb1c4899c594a9
mwinter80 <mwinter@unboundedpress.org>
2015-11-05 10:35:17 -0800

    removing askbot

--- askbot/management/commands/clear_cache.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import NoArgsCommand
-from django.core.cache import cache
-
-class Command(NoArgsCommand):
-    def handle_noargs(self, *args, **kwargs):
-        cache.clear()
