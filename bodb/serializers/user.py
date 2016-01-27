e1b47fc6981721406c72c028b08abbf392530dc5
mwinter80 <mwinter@unboundedpress.org>
2016-01-27 11:16:23 -0800

    url changes

--- bodb/serializers/user.py
+++
@@ -1,9 +0,0 @@
-from django.forms import widgets
-from rest_framework import serializers
-from django.contrib.auth.models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = User
-        fields = ('id', 'username',)
