165483a9005ff5da23b91aa822aa742cb4e9cc95
mwinter80 <mwinter@unboundedpress.org>
2016-07-27 11:37:45 -0700

    adding search_indexes.py

---
+++ bodb/search_indexes.py
@@ -0,0 +1,25 @@
+import datetime
+from haystack import indexes
+from bodb.models import *
+
+
+class DocumentIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Document
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
+    
+    
+class SEDIndex(DocumentIndex):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return SED
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
