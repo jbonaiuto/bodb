981d8cb89a8ce4dc187a32bbe251f06adbff7d17
jbonaiuto <jbonaiuto@gmail.com>
2016-09-23 16:34:50 +0100

    export figures to png. fixes #145

--- uscbp/image_utils.py
+++ uscbp/image_utils.py
@@ -4,15 +4,15 @@ import os
 def get_thumbnail(image_path, w, h):
     new_size=scale(w,h,300,30000,True)
     image_dir, image_name = os.path.split(image_path)
-    thumb_name = "%s-%sx%s.jpg" % (os.path.splitext(image_name)[0], new_size[0], new_size[1])
+    thumb_name = "%s-%sx%s.png" % (os.path.splitext(image_name)[0], new_size[0], new_size[1])
     thumb_path = os.path.join(image_dir, thumb_name)
     if not os.path.exists(thumb_path):
         image = Image.open(image_path)
-        if image.mode not in ("L", "RGB"):
-            image = image.convert("RGB")
+#        if image.mode not in ("L", "RGB"):
+#            image = image.convert("RGB")
         try:
-            image.thumbnail(new_size, Image.ANTIALIAS)
-            image.save(thumb_path, "JPEG", quality=100)
+            image.thumbnail(new_size)#, Image.ANTIALIAS)
+            image.save(thumb_path, "PNG", quality=100)
         except Exception,e:
             print(str(e))
             return image_path
