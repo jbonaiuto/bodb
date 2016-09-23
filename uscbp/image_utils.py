e796f00a7d1ae03cc6f07c0ae5fc60336f18d26c
jbonaiuto <jbonaiuto@gmail.com>
2016-09-23 16:39:03 +0100

    tried to fix image pdf export

--- uscbp/image_utils.py
+++ uscbp/image_utils.py
@@ -6,16 +6,16 @@ def get_thumbnail(image_path, w, h):
     image_dir, image_name = os.path.split(image_path)
     thumb_name = "%s-%sx%s.png" % (os.path.splitext(image_name)[0], new_size[0], new_size[1])
     thumb_path = os.path.join(image_dir, thumb_name)
-    if not os.path.exists(thumb_path):
-        image = Image.open(image_path)
+    #if not os.path.exists(thumb_path):
+    image = Image.open(image_path)
 #        if image.mode not in ("L", "RGB"):
 #            image = image.convert("RGB")
-        try:
-            image.thumbnail(new_size)#, Image.ANTIALIAS)
-            image.save(thumb_path, "PNG", quality=100)
-        except Exception,e:
-            print(str(e))
-            return image_path
+    try:
+        image.thumbnail(new_size)#, Image.ANTIALIAS)
+        image.save(thumb_path, "PNG", quality=100)
+    except Exception,e:
+        print(str(e))
+        return image_path
     return thumb_path
 
 def scale(w, h, x, y, maximum=True):
